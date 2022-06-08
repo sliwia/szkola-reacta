e759aaebb69b90d0f8103b3aa9e775dba123207c
Liwia Szajner <liwiaszajner@macbook-pro-liwia.home>
2022-06-08 22:03:23 +0200

    module 3 exercises 1-6

--- module_3/third-project-app/src/App.js
+++ module_3/third-project-app/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCoffee } from "@fortawesome/free-solid-svg-icons"
 
 import { Input, Button, Textarea } from "./components/Form"
 import Card from "./components/Card";
@@ -29,22 +29,22 @@ function App() {
       <Button icon={<FontAwesomeIcon icon={faCoffee} />} label='My button' bgColor='grey' color='white' />
       <br />
       <br />
-      <Input bgColor='lightgrey' color="black" borderSize='1px' borderRadius='4px' borderColor="blue" />
+      <Input bgColor='lightgrey' color="black" borderSize='1px' borderRadius='4px' borderColor='blue' />
       <br />
       <br />
-      <Textarea bgColor='lightgrey' color="black" borderSize='1px' borderRadius='4px' borderColor="purple" />
+      <Textarea bgColor='lightgrey' color='black' borderSize='1px' borderRadius='4px' borderColor='purple' />
       <br />
       <br />
       <Card
-        title="Shrim and Chorizo Paella"
-        intro="This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like." showMore content="Method: Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10 minutes"
+        title='Shrim and Chorizo Paella'
+        intro='This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like. Method: Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10 minutes'
       />
       <br />
       <br />
       <Menu>
-        <MenuItem label="Link 1" />
-        <MenuItem label="Link 2" />
-        <MenuItem isActive label="Link 3" />
+        <MenuItem label='Link 1' />
+        <MenuItem label='Link 2' />
+        <MenuItem isActive label='Link 3' />
       </Menu>
       <br />
       <br />
@@ -54,8 +54,8 @@ function App() {
           fullName={value.fullName}
           age={value.age}
           occupation={value.occupation}
-
-        />)
+        />
+      )
       }
     </div>
   );
